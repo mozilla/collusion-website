021be4d0ea8c81f405a27dce207555901761561a
Mavis Ou <mmmavisss@gmail.com>
2013-02-15 15:25:28 -0800

    get dropdown options directly from database table. kinda a mess. but it's working. lol

--- mavis.js
+++ mavis.js
@@ -3,6 +3,9 @@ var jsonVar = require("./jsonVar.js");
 var app = express();
 var pg = require("pg");
 
+var columnInfo = {};
+var insertPrefix = "";
+
 //app.use(express.static(__dirname + '/public'));
 
 app.configure(function() {
@@ -10,13 +13,28 @@ app.configure(function() {
     app.use(express.bodyParser());    
 });
 
+getColumnInfo('connections', function(tempColumnInfo){
+  columnInfo = tempColumnInfo;
+  //console.log(columnInfo);
+  function queryInsertPrefix(){
+    var excludePrimaryKey = Object.keys(columnInfo).slice(1);
+    var temp = excludePrimaryKey.join(",");
+    insertPrefix = "INSERT into connections(" + temp + ") VALUES ";
+  }
+  queryInsertPrefix();
+});
+
+
+
 /* index page ============================================================ */
 app.get('/', function(req, res){
-  var client = new pg.Client(process.env.DATABASE_URL);
   var jsonConnectionString = jsonVar.jsonConnectionString;
   var connectionArray = JSON.parse(jsonConnectionString);
-  var foo = "";
-  
+  var printedRow = "";
+
+/*
+  var client = new pg.Client(process.env.DATABASE_URL);
+
   client.connect(function(err) {
     if (err) console.log(err);
   });
@@ -24,27 +42,36 @@ app.get('/', function(req, res){
   client.query("drop table connections");
   client.query("CREATE TABLE connections( id SERIAL PRIMARY KEY, source varchar(100), target varchar(100), timestamp timestamp, contentType varchar(50), cookie boolean, sourceVisited boolean, secure boolean, sourcePathDepth int, sourceQueryDepth int )");
   
-  for (var i=0; i<connectionArray.length; i++){
-    foo = foo + connectionArray[i] + "<br/>";
-    queryInserte(connectionArray[i]);
-    client.query(queryInserte(connectionArray[i]));
-  }
+  //client.end();
     
-  client.end();
-  
+  client.on('drain', client.end.bind(client)); //disconnect client when all queries are finished
+*/
   
   getTableColumns("connections", function(arr){
-    console.log(arr);
+  /* 
+    var client2 = new pg.Client(process.env.DATABASE_URL);
+    client2.connect(function(err) {
+      if (err) console.log(err);
+    });
+    for (var i=0; i<connectionArray.length; i++){
+      printedRow = printedRow + connectionArray[i] + "<br/>";
+      //queryInsert(connectionArray[i]);
+      client2.query(queryInsert(connectionArray[i]));
+      console.log(queryInsert(connectionArray[i]));
+    }
+    client2.on('drain', client2.end.bind(client2));
+  */
+    
     var selectForm = 
           '<form action="/selectRows" enctype="multipart/form-data" method="get"> Select * From '+
           '<input type="text" name="tableName" value="connections"> Where&nbsp'+
-          '&nbsp<input type="text" name="fieldName" value="id">&nbsp = '+
+          //'&nbsp<input type="text" name="fieldName" value="id">&nbsp = '+
+          '<select name="fieldName">' + arr + '</select>&nbsp = ' +
           '&nbsp<input type="text" name="fieldValue" value="1">&nbsp'+
           //'<input type="submit" value="Submit">'+
           '<button id="submit">Submittt</button>' +
           '</form>' +
-          '======================================== </br></br>' +
-          '<select>' + arr + '</select></br></br>'
+          '======================================== </br></br>'
           ;
     showQueryResult("select * from connections", res, selectForm);
   });
@@ -56,13 +83,13 @@ app.get('/', function(req, res){
 /* selectRows ============================================================ */
 app.get('/selectRows', function(req, res) {
   console.log(req.query);
-  for ( var hello in req.query ){
-    console.log(hello + " : " + req.query[hello]);
-  }
+//  for ( var hello in req.query ){
+//    console.log(hello + " : " + req.query[hello]);
+//  }
   showQueryResult(querySelect(req.query), res);
 })
 
-
+/* getTableColumns ======================================================== */
 function getTableColumns(tbl, callback){
   var client = new pg.Client(process.env.DATABASE_URL);
   
@@ -81,9 +108,8 @@ function getTableColumns(tbl, callback){
   
   query.on('end', function() {
     client.end();
-    var dropdown = "";
+    var dropdown;
     arr.forEach(function(columnName, i){
-      console.log(columnName+"===="+i);
       dropdown = dropdown + "<option value='" + columnName + "'>" + columnName + "</option>";
     });
     callback(dropdown);
@@ -91,9 +117,36 @@ function getTableColumns(tbl, callback){
   
 }
 
+/* getColumnInfo ==================================================== */
+function getColumnInfo(tbl, callback){
+  var client = new pg.Client(process.env.DATABASE_URL);
+  
+  client.connect(function(err) {
+    if (err) console.log(err);
+  });
+  
+  var queryString = "select column_name, data_type from information_schema.columns WHERE table_name = " + "'" + tbl + "'";
+  var tempColumnInfo = {};
+
+  var query = client.query(queryString, function(err, result){
+    for (var i=0; i<result.rows.length; i++){
+      var tempColumnName = result.rows[i].column_name;
+      var tempDataType = result.rows[i].data_type;
+      tempColumnInfo[tempColumnName] = tempDataType;
+    }
+  });
+  
+  query.on('end', function() {
+    client.end();
+    callback(tempColumnInfo);
+  });
+}
+
+
 
 /* Print query result ================================================== */
 function showQueryResult(query, res, extraMessage){
+
   var client = new pg.Client(process.env.DATABASE_URL);
   var printOnScreen = (extraMessage || "") + query + "<br/><br/>";
   
@@ -105,12 +158,13 @@ function showQueryResult(query, res, extraMessage){
   myQuery.on('error', function(error) {
     if (error) console.log("=== ERRORRR === " + error);
   });
-
+var omg = 1;
   myQuery.on('row', function(row) {
     var rowProperties = new Array();
     for (var prop in row){
       rowProperties.push(row[prop]);
     }
+    console.log("++++++++++++Row "+omg++);
     printOnScreen = printOnScreen + rowProperties.join(", ") + "<br/>";
   });
 
@@ -122,9 +176,11 @@ function showQueryResult(query, res, extraMessage){
 }
 
 
+
+
 /* Generates INSERT INTO query ========================================= */
-function queryInserte(obj){
-  var prefix = "INSERT into connections(source, target, timestamp, contenttype, cookie, sourcevisited, secure, sourcepathdepth, sourcequerydepth) VALUES ";
+function queryInsert(obj){
+  var prefix = insertPrefix;
   var valuesArr = new Array();
   for (var i=0; i<obj.length; i++){
     if (i==0 || i==1 || i==3){ // hardcoded.  BAD!
@@ -137,15 +193,22 @@ function queryInserte(obj){
     }
   }
   
-  var queryInsert = prefix + "(" + valuesArr.join(",") + ")";
+  var queryInsertString = prefix + "(" + valuesArr.join(",") + ")";
+  //console.log("queryInsert" + queryInsert);
   
-  return queryInsert;
+  return queryInsertString;
 }
 
 
 /* querySelect ======================================================= */
 function querySelect(filter){
-  return "SELECT * FROM " + filter.tableName + " WHERE " + filter.fieldName + " = " + filter.fieldValue;
+  var fieldName = filter.fieldName;
+  var fieldValue = filter.fieldValue;
+  var data_type = columnInfo[filter.fieldName];
+  if ( data_type.match("char") ){ fieldValue = "'" + fieldValue + "'"; }
+  if ( data_type.match("timestamp") ){ fieldValue = "to_date('" + fieldValue + "','Mon DD YYYY')"}
+  
+  return "SELECT * FROM " + filter.tableName + " WHERE " + fieldName + " = " + fieldValue;
 }
 
 
