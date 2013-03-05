330b1e2d722aedeb7f10f8a4e8d5349eabdd0b3b
Mavis Ou <mmmavisss@gmail.com>
2013-03-05 10:26:15 -0800

    changed indentation from 2 spaces to 4 spaces; code cleanup

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ var https = require("https");
 
 
 app.configure(function(){
-  app.set("view engine", "handlebars");
-  app.set("view options", { layout: false });
-  app.set("views", __dirname + "/");
-  app.engine("html", cons.handlebars);
-  app.use(express.static(__dirname + "/public"));
-  app.use(express.bodyParser());    
+    app.set("view engine", "handlebars");
+    app.set("view options", { layout: false });
+    app.set("views", __dirname + "/");
+    app.engine("html", cons.handlebars);
+    app.use(express.static(__dirname + "/public"));
+    app.use(express.bodyParser());    
 });
 
 
 
 handlebars.registerHelper('avatarBox', function(items, options) {
-  var boxes = "";
-  for(var i=0, l=items.length; i<l; i++) {
-    boxes = boxes + options.fn(items[i]);
-  }
-  return boxes;
+    var boxes = "";
+    for(var i=0, l=items.length; i<l; i++) {
+        boxes = boxes + options.fn(items[i]);
+    }
+    return boxes;
 });
 
 
@@ -31,8 +31,8 @@ handlebars.registerHelper('avatarBox', function(items, options) {
 *   Index page
 */
 app.get("/", function(req, res){
-  //postData(res);
-  res.render("index.html");
+    //postData(res);
+    res.render("index.html");
 });
 
 
@@ -40,7 +40,7 @@ app.get("/", function(req, res){
 *   Donate data button handler
 */
 app.post("/donate", function(req, res){
-  postData(res);
+    postData(res);
 });
 
 
@@ -48,8 +48,8 @@ app.post("/donate", function(req, res){
 *   TESTING TO SEE IF THE CODE WORKS WITH THE NEW DATABASE SERVER
 */
 app.get("/foo", function(req,res){
-  postData(res);
-  //getData(res);
+    postData(res);
+    //getData(res);
 });
 
 
@@ -57,75 +57,75 @@ app.get("/foo", function(req,res){
 *   Browse data page
 */
 app.get("/browse-data", function(req, res){
-  var query = {};
-  query.trackersQuery = "SELECT target, COUNT(distinct source) FROM connections GROUP BY target ORDER BY COUNT(distinct source) DESC LIMIT 5";
-  query.websitesQuery = "SELECT source, COUNT(distinct target), MAX(timestamp) FROM connections where sourceVisited = true GROUP BY source ORDER BY COUNT(distinct target) DESC";
+    var query = {};
+    query.trackersQuery = "SELECT target, COUNT(distinct source) FROM connections GROUP BY target ORDER BY COUNT(distinct source) DESC LIMIT 5";
+    query.websitesQuery = "SELECT source, COUNT(distinct target), MAX(timestamp) FROM connections where sourceVisited = true GROUP BY source ORDER BY COUNT(distinct target) DESC";
 
-  var queryString = JSON.stringify(query);
+    var queryString = JSON.stringify(query);
   
-  var options = {
-    hostname: "collusiondb-development.herokuapp.com",
-    path: "/getBrowseData",
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Length": queryString.length
-    }
-  };
+    var options = {
+        hostname: "collusiondb-development.herokuapp.com",
+        path: "/getBrowseData",
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Content-Length": queryString.length
+        }
+    };
   
-  var trackerBoxes = new Array();
-  var websiteBoxes = new Array();
-  var result = "";
-  var reqGet = http.request(options, function(response) {
-    response.setEncoding("utf8");
-    response.on("data", function (chunk) {
-      result += chunk;
+    var trackerBoxes = new Array();
+    var websiteBoxes = new Array();
+    var result = "";
+    var reqGet = http.request(options, function(response) {
+        response.setEncoding("utf8");
+        response.on("data", function (chunk) {
+          result += chunk;
+        });
+        response.on("end", function(){
+            result = JSON.parse(result);
+            for (var i=0; i<result.trackers.length; i++ ){
+            var row = result.trackers[i];
+            var infoUrl = row[ Object.keys(row)[0] ];
+            var infoLine1 = row[ Object.keys(row)[1] ];
+            var url = "/trackers/" + infoUrl;
+            trackerBoxes.push(
+                {
+                    url: url,
+                    infoUrl: infoUrl,
+                    faviconUrl: "http://" + infoUrl +  "/favicon.ico",
+                    infoLine1: infoLine1,
+                    infoLine2: row[ Object.keys(row)[2] ]
+                });
+            }
+            for (var i=0; i<result.websites.length; i++ ){
+            var row = result.websites[i];
+            var infoUrl = row[ Object.keys(row)[0] ];
+            var infoLine1 = row[ Object.keys(row)[1] ];
+            var url = "/websites/" + infoUrl;
+            websiteBoxes.push(
+                {
+                    url: url,
+                    infoUrl: infoUrl,
+                    faviconUrl: "http://" + infoUrl +  "/favicon.ico",
+                    infoLine1: infoLine1,
+                    infoLine2: row[ Object.keys(row)[2] ]
+                });
+            }
+            var data = {
+                trackers: trackerBoxes,
+                websites: websiteBoxes
+            }
+            res.render("browse-data.html", data);
+        });
     });
-    response.on("end", function(){
-        result = JSON.parse(result);
-        for (var i=0; i<result.trackers.length; i++ ){
-        var row = result.trackers[i];
-        var infoUrl = row[ Object.keys(row)[0] ];
-        var infoLine1 = row[ Object.keys(row)[1] ];
-        var url = "/trackers/" + infoUrl;
-        trackerBoxes.push(
-          {
-            url: url,
-            infoUrl: infoUrl,
-            faviconUrl: "http://" + infoUrl +  "/favicon.ico",
-            infoLine1: infoLine1,
-            infoLine2: row[ Object.keys(row)[2] ]
-          });
-        }
-        for (var i=0; i<result.websites.length; i++ ){
-        var row = result.websites[i];
-        var infoUrl = row[ Object.keys(row)[0] ];
-        var infoLine1 = row[ Object.keys(row)[1] ];
-        var url = "/websites/" + infoUrl;
-        websiteBoxes.push(
-          {
-            url: url,
-            infoUrl: infoUrl,
-            faviconUrl: "http://" + infoUrl +  "/favicon.ico",
-            infoLine1: infoLine1,
-            infoLine2: row[ Object.keys(row)[2] ]
-          });
-        }
-        var data = {
-        trackers: trackerBoxes,
-        websites: websiteBoxes,
-        }
-        res.render("browse-data.html", data);
+
+    reqGet.on("error", function(e) {
+        console.log("Problem with GET request: " + e.message);
     });
-  });
 
-  reqGet.on("error", function(e) {
-    console.log("Problem with GET request: " + e.message);
-  });
-
-  // write data to request body
-  reqGet.write(queryString);
-  reqGet.end();
+    // write data to request body
+    reqGet.write(queryString);
+    reqGet.end();
     
 });
 
@@ -135,56 +135,54 @@ app.get("/browse-data", function(req, res){
 */
 //app.param('tracker', /^\d+$/);
 app.get("/trackers/:tracker", function(req, res){
-  var query = {"target": req.params.tracker};
-  var queryString = JSON.stringify(query);
+    var query = {"target": req.params.tracker};
+    var queryString = JSON.stringify(query);
   
-  var options = {
-    hostname: "collusiondb-development.herokuapp.com",
-//    hostname: "localhost",
-//    port: 7000,
-    path: "/getTracker",
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Length": queryString.length
-    }
-  };
+    var options = {
+        hostname: "collusiondb-development.herokuapp.com",
+        path: "/getTracker",
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Length": queryString.length
+        }
+    };
   
-  var result = ""
-  var getReq = http.request(options, function(response) {
-    response.setEncoding("utf8");
-    response.on("data", function (chunk) {
-       result += chunk;
+    var result = ""
+    var getReq = http.request(options, function(response) {
+        response.setEncoding("utf8");
+        response.on("data", function (chunk) {
+            result += chunk;
+        });
+        response.on("end", function(){
+            result = JSON.parse(result);
+            var wrapper = "<ul>";
+            for (var i=0; i<result.rowCount; i++ ){
+                var row = result.rows[i];
+                var rowProperties = new Array();
+                for (var prop in row){
+                    rowProperties.push(row[prop]);
+                }
+                var url = "/websites/" + rowProperties[0];
+                var anchor = "<a href='" + url +  "'>" + rowProperties[0] + "</a>";
+                wrapper = wrapper + "<li cookie-connection=" + rowProperties[1] + ">" + anchor + "</li>";
+            }
+            wrapper += "</ul>";
+            var data = {
+              tracker: req.params.tracker,
+              details: wrapper
+            };
+            res.render("trackerInfo.html", data);
+        });
     });
-    response.on("end", function(){
-        result = JSON.parse(result);
-        var wrapper = "<ul>";
-        for (var i=0; i<result.rowCount; i++ ){
-        var row = result.rows[i];
-        var rowProperties = new Array();
-        for (var prop in row){
-          rowProperties.push(row[prop]);
-        }
-        var url = "/websites/" + rowProperties[0];
-        var anchor = "<a href='" + url +  "'>" + rowProperties[0] + "</a>";
-        wrapper = wrapper + "<li cookie-connection=" + rowProperties[1] + ">" + anchor + "</li>";
-        }
-        wrapper += "</ul>";
-        var data = {
-        tracker: req.params.tracker,
-        details: wrapper
-        };
-        res.render("trackerInfo.html", data);
+
+    getReq.on("error", function(e) {
+        console.log("Problem with request: " + e.message);
     });
-  });
 
-  getReq.on("error", function(e) {
-    console.log("problem with request: " + e.message);
-  });
-
-  // write data to request body
-  getReq.write(queryString);
-  getReq.end();
+    // write data to request body
+    getReq.write(queryString);
+    getReq.end();
 
 });
 
@@ -193,63 +191,60 @@ app.get("/trackers/:tracker", function(req, res){
 *   Website details
 */
 app.get("/websites/:website", function(req, res){
-  var query = {"source": req.params.website};
-  var queryString = JSON.stringify(query);
-  
-  var options = {
-    hostname: "collusiondb-development.herokuapp.com",
-//    hostname: "localhost",
-//    port: 7000,
-    path: "/getVisitedWebsite",
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Length": queryString.length
-    }
-  };
-  
-  var result = "";
-  var getReq = http.request(options, function(response) {
-    response.setEncoding("utf8");
-    response.on("data", function (chunk) {
-       result += chunk;
-    });
-    response.on("end", function(){
-        result = JSON.parse(result);
-        var wrapper = "<ul>";
-        for (var i=0; i<result.rowCount; i++ ){
-        var row = result.rows[i];
-        var rowProperties = new Array();
-        for (var prop in row){
-          rowProperties.push(row[prop]);
+    var query = {"source": req.params.website};
+    var queryString = JSON.stringify(query);
+
+    var options = {
+        hostname: "collusiondb-development.herokuapp.com",
+        path: "/getVisitedWebsite",
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Content-Length": queryString.length
         }
-        var url = "/trackers/" + rowProperties[0];
-        var anchor = "<a href='" + url +  "'>" + rowProperties[0] + "</a>";
-        wrapper = wrapper + "<li cookie-connection=" + rowProperties[1] + ">" + anchor + "</li>";
-        }
-        wrapper += "</ul>";
-        var data = {
-        website: req.params.website,
-        details: wrapper
-        };
-        res.render("websiteInfo.html", data);
-    });
-  });
-
-  getReq.on("error", function(e) {
-    console.log("problem with request: " + e.message);
-  });
-
-  // write data to request body
-  getReq.write(queryString);
-  getReq.end();
-
+    };
   
+    var result = "";
+    var getReq = http.request(options, function(response) {
+        response.setEncoding("utf8");
+        response.on("data", function (chunk) {
+            result += chunk;
+        });
+        response.on("end", function(){
+            result = JSON.parse(result);
+            var wrapper = "<ul>";
+            for (var i=0; i<result.rowCount; i++ ){
+                var row = result.rows[i];
+                var rowProperties = new Array();
+                for (var prop in row){
+                  rowProperties.push(row[prop]);
+                }
+                var url = "/trackers/" + rowProperties[0];
+                var anchor = "<a href='" + url +  "'>" + rowProperties[0] + "</a>";
+                wrapper = wrapper + "<li cookie-connection=" + rowProperties[1] + ">" + anchor + "</li>";
+            }
+            wrapper += "</ul>";
+            var data = {
+                website: req.params.website,
+                details: wrapper
+            };
+            res.render("websiteInfo.html", data);
+        });
+    });
+
+    getReq.on("error", function(e) {
+        console.log("problem with request: " + e.message);
+    });
+
+    // write data to request body
+    getReq.write(queryString);
+    getReq.end();
+
 });
 
 
 /**************************************************
-*   Post data
+*   Post data (testing purpose)
 */
 function postData(res){
     // sample data
@@ -258,68 +253,66 @@ function postData(res){
     var postDataString = JSON.stringify(postData);
     
     var options = {
-      hostname: "collusiondb-development.herokuapp.com",
-      // hostname: "localhost",
-//       port: 7000,
-      path: "/donateData",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Content-Length": postDataString.length
-      }
+        hostname: "collusiondb-development.herokuapp.com",
+        path: "/donateData",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Content-Length": postDataString.length
+        }
     };
     
     var postReq = http.request(options, function(response) {
-      response.setEncoding("utf8");
-      response.on("data", function (chunk) {
-        console.log("POST data response: " + chunk);
-        res.send("POST data response: " + chunk);
-      });
+        response.setEncoding("utf8");
+        response.on("data", function (chunk) {
+            console.log("POST data response: " + chunk);
+            res.send("POST data response: " + chunk);
+        });
     });
 
     postReq.on("error", function(e) {
-      console.log("problem with request: " + e.message);
+       console.log("problem with request: " + e.message);
     });
 
     // write data to request body
     postReq.write(postDataString);
     postReq.end();
-  }
+}
 
 
 /**************************************************
-*   Get data
+*   Get data (testing purpose)
 */
 function getData(res){
-  var query = {"source": "www.google.com"};
-  var queryString = JSON.stringify(query);
-  console.log(queryString);
+    var query = {"source": "www.google.com"};
+    var queryString = JSON.stringify(query);
+    console.log(queryString);
   
-  var options = {
-    hostname: "collusiondb-development.herokuapp.com",
-    path: "/getData",
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Length": queryString.length
-    }
-  };
+    var options = {
+        hostname: "collusiondb-development.herokuapp.com",
+        path: "/getData",
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Content-Length": queryString.length
+        }
+    };
   
-  var getReq = http.request(options, function(response) {
-    response.setEncoding("utf8");
-    response.on("data", function (chunk) {
-      console.log("GET data response: " + chunk);
-      res.send("GET data response: " + chunk);
+    var getReq = http.request(options, function(response) {
+        response.setEncoding("utf8");
+        response.on("data", function (chunk) {
+            console.log("GET data response: " + chunk);
+            res.send("GET data response: " + chunk);
+        });
     });
-  });
 
-  getReq.on("error", function(e) {
-    console.log("problem with request: " + e.message);
-  });
+    getReq.on("error", function(e) {
+        console.log("problem with request: " + e.message);
+    });
 
-  // write data to request body
-  getReq.write(queryString);
-  getReq.end();
+    // write data to request body
+    getReq.write(queryString);
+    getReq.end();
 };
 
 
@@ -328,5 +321,5 @@ function getData(res){
 
 
 app.listen(process.env.PORT, function() {
-  console.log("Listening on " + process.env.PORT);
+    console.log("Listening on " + process.env.PORT);
 });
