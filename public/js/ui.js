83a8d069e8016414db33a6b126fe357736154ab4
Mavis Ou <mmmavisss@gmail.com>
2013-10-01 17:12:18 -0700

    temp changes for Summit

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,5 +1,4 @@
-// const DATABASE_URL = "http://collusiondb-development.herokuapp.com";
-const DATABASE_URL = "http://localhost:7000";
+const DATABASE_URL = "http://collusiondb-development.herokuapp.com";
 const ROWS_PER_TABLE_PAGE = 20;
 var currentPage;
 var allSites;
